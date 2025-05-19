aef5d5cf4696a7141676e952c8167ab4b90dd351
bikfxyz <bikfxyz@qq.com>
2025-05-20 00:11:04 +0800

    添加uv安装包

--- Tools/downloadPypiWin64.bat
+++ Tools/downloadPypiWin64.bat
@@ -1,3 +1,3 @@
-python -m pip download --only-binary=:all: --platform=win_amd64 --python-version=310 -d ./packages4py310Win64 -r .\requirements_win-new.txt 
+python -m pip download --only-binary=:all: --platform=win_amd64 --python-version=310 -d ./packages4py310Win64 -r ..\requirements_win.txt 
 @echo #python -m pip download -d ./path pyinstaller -i https://pypi.mirrors.ustc.edu.cn/simple/ --trusted-host pypi.mirrors.ustc.edu.cn
 @echo #python -m pip install -r requirements.txt -i http://pypi.douban.com/simple/ --trusted-host pypi.douban.com
---
+++ Tools/requirementsInstall_win.bat
@@ -0,0 +1,1 @@
+uv pip install -r ..\requirements_win.txt
